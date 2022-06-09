47c6af6355c1da85a6f366003031c21ea3b6e154
Kanasz Mihai <106661978+KanaszM@users.noreply.github.com>
2022-06-09 12:34:53 +0300

    Update source.gd

--- source.gd
+++ source.gd
@@ -1,3 +1,4 @@
+### FILE MANIPULATION METHODS ###
 # Get a list of complete filepaths starting from the specified _dir_path.
 # You can use the _filter argument to get files of a specific extension.
 # Recursive mode can be enabled/disabled with the _recursive argument.
@@ -22,8 +23,8 @@ static func Get_Files(_dir_path: String, _filter: PoolStringArray = [], _recursi
 			_dir.list_dir_end()
 	return _result
 
-# This function will convert an .rtf file to an .gd file ready to be used in Godot
-# The optional _open argument will open de output file upon completion
+# This function will convert an .rtf file to an .gd file ready to be used in Godot.
+# The optional _open argument will open de output file upon completion.
 static func RTF_To_GD(_path_source_file: String, _path_output_DIR: String, _open: bool) -> void:
 	var _source_file: File = File.new()
 	var _output_file: File = File.new()
@@ -41,3 +42,13 @@ static func RTF_To_GD(_path_source_file: String, _path_output_DIR: String, _open
 			_source_file.close()
 			_output_file.close()
 			if _open: OS.shell_open(_output_path_file)
+
+# This function will delete all files from _dir_path if their extensions match the file extensions specified in the _file_extensions_to_clear argument.
+# Recursive mode can be enabled/disabled with the _recursive argument.
+static func Clear_Temp_Files(_dir_path: String, _file_extensions_to_clear: PoolStringArray, _recursive: bool = false) -> String:
+	var _temp_dir: Directory = Directory.new()
+	var _temp_files: PoolStringArray = Get_Files(_dir_path, _file_extensions_to_clear, _recursive)
+	var _temp_clear_count: int = 0
+	for _file_path in _temp_files:
+		if _temp_dir.remove(_file_path) == OK: _temp_clear_count += 1
+	return str("Cleared: " + str(_temp_clear_count) + " / " + str(_temp_files.size()))
