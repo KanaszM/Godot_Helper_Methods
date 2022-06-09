fd6cebe528c2ec677e357babf20680f091550ab4
Kanasz Mihai <106661978+KanaszM@users.noreply.github.com>
2022-06-09 12:47:22 +0300

    Update km_vbs.gd

--- km_vbs.gd
+++ km_vbs.gd
@@ -12,7 +12,7 @@ static func Convert_Excel_File(_source_path: String, _destination_dir: String, _
 		if not _destination_extension.empty():
 			var _destination_path_copy: String = _destination_dir.plus_file(_source_filename + "." + _source_extension)
 			var _destination_path_save: String = _destination_dir.plus_file(_source_filename + _destination_extension)
-			var _vbs_script: PoolStringArray = [
+			var _vbs_script: PoolStringArray = PoolStringArray([
 				"On Error Resume Next",
 				"Process()",
 				"Function Process",
@@ -39,7 +39,7 @@ static func Convert_Excel_File(_source_path: String, _destination_dir: String, _
 				"	oExcel.Quit",
 				"	Set oExcel = Nothing",
 				"	Exit Function",
-				"End Function"]
+				"End Function"])
 			var _vbs_file_path: String = OS.get_user_data_dir().plus_file("_csv_" + _source_filename + ".vbs")
 			var _vbs_file: File = File.new()
 			if _vbs_file.open(_vbs_file_path, _vbs_file.WRITE) == OK:
@@ -48,3 +48,51 @@ static func Convert_Excel_File(_source_path: String, _destination_dir: String, _
 				OS.shell_open(_vbs_file_path)
 				_result = true
 	return _result
+
+# This function will create a .vbs script that will open the directory specified in the _source_path argument
+static func Open_Dir(_source_path: String) -> bool:
+	var _result: bool = false
+	var _vbs_file_path: String = OS.get_user_data_dir().plus_file("_dir_" + _source_path + ".vbs")
+	var _vbs_source_path: String = _source_path.replace("/", "\\") + ("\\" if _source_path[_source_path.length() - 1] != "\\" else "")
+	var _vbs_script: PoolStringArray = PoolStringArray([
+		"Dim oShell", "Set oShell = wscript.CreateObject(\"Shell.Application\")",
+		"oShell.Open \"" + _vbs_source_path + "\"",
+		"Set oShell = Nothing", "WScript.Quit"])
+	var _vbs_file: File = File.new()
+	if _vbs_file.open(_vbs_file_path, _vbs_file.WRITE) == OK:
+		for _vbs_line in _vbs_script: _vbs_file.store_line(_vbs_line)
+		_vbs_file.close()
+		OS.shell_open(_vbs_file_path)
+		_result = true
+	return _result
+
+# This function will create a .vbs script that will open the specified file in the _source_path argument in Microsoft Windows Notepad
+static func Open_With_Notepad(_source_path: String) -> void:
+	var _vbs_file_path: String = OS.get_user_data_dir().plus_file("_notepad_" + _source_path.get_file().get_slice(".", 0) + ".vbs")
+	var _file: File = File.new()
+	if _file.open(_vbs_file_path, _file.WRITE) == OK:
+		var _vbs_path: String = _source_path.replace("/", "\\")
+		var _script: PoolStringArray = PoolStringArray([
+			"With CreateObject(\"WScript.Shell\")",
+			"	.Run \"notepad.exe " + _vbs_path + "\"",
+			"End With",
+			"Set oShell = Nothing", "WScript.Quit"])
+		for _line in _script: _file.store_line(_line)
+		_file.close()
+		OS.shell_open(_vbs_file_path)
+
+# This function will create a .vbs script that will open the specified file in the _source_path argument with a delay set in the _delay argument.
+static func Open_With_Delay(_source_path: String, _delay: int = 1000) -> void:
+	var _vbs_file_path: String = OS.get_user_data_dir().plus_file("_delay_" + _source_path.get_file().get_slice(".", 0) + ".vbs")
+	var _file: File = File.new()
+	if _file.open(_vbs_file_path, _file.WRITE) == OK:
+		var _vbs_path: String = _source_path.replace("/", "\\")
+		var _script: PoolStringArray = PoolStringArray([
+			"WScript.sleep " + str(_delay),
+			"Dim oShell", "Set oShell = WScript.CreateObject(\"WScript.Shell\")",
+#			"Dim oExec", "Set oExec = oShell.Exec(\"" + _vbs_path + "\")"] # Alternative method
+			"oShell.Run \"" + _vbs_path + "\"",
+			"Set oShell = Nothing", "WScript.Quit"])
+		for _line in _script: _file.store_line(_line)
+		_file.close()
+		OS.shell_open(_vbs_file_path)
